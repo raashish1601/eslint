2b7f7864964da9dc61c5f75b783595f5e2e56bb7
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2026-03-28 15:58:37 +0530

    fix: add es2015 lib reference to bundled types

---
+++ tests/lib/types/es5-types.test.ts
@@ -0,0 +1,9 @@
+import type { ESLint, Linter, SourceCode } from "eslint";
+
+declare const config: Linter.Config;
+declare const options: ESLint.Options;
+declare const sourceCode: SourceCode;
+
+config;
+options;
+sourceCode;
